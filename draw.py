d51bd154fef9be7ff89abed891a576306183d363
Sanyinmai <165164328+Sanyinmai@users.noreply.github.com>
2026-05-06 16:32:41 +0800

    Update draw.py

--- draw.py
+++ draw.py
@@ -4,13 +4,12 @@ import numpy as np
 import os.path
 plt.rcParams['font.family'] = ['sans-serif']
 plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False  # 修复负号问题
+plt.rcParams['axes.unicode_minus'] = False
 
 # 数据集
-dataset = 'forth87-108(10-26)'
-# dataset = '108-137(11-15)'
+dataset = ''
 # 输出文件夹
-output = './output/'
+output = ''
 
 # 频段数据
 # frequency_band = dataset.split('(')[0]  # 87-108
@@ -18,11 +17,11 @@ output = './output/'
 # frequency_end = int(frequency_band.split('-')[1])  # 108
 
 # 读取文件路径
-read_path = 'F:\spectrum_data\/' + dataset + '/'
+read_path = ''
 # 保存文件路径
-save_path = 'F:\spectrum_data\TrainSp256\TrainSp256/'
+save_path = ''
 #图像参数文件保存路径
-# param_save_path = r'F:\spectrum_data\图片参数标签/'
+# param_save_path = r''
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
@@ -44,7 +43,7 @@ def read_data(n):
         for _ in range(7):
             res = []
             for i in range(read_line):
-                line = f.readline().strip()#跳过了第一行
+                line = f.readline().strip()
                 line_data = line.split(' ;')
                 data = line_data[0].split(' ')
                 data = data[584:840]
@@ -58,7 +57,6 @@ def read_data(n):
             for _ in range(skip_line):
                 f.readline()
     return totalres
-    # totaltimeRecord
 
 def draw(n):
     """
@@ -69,7 +67,7 @@ def draw(n):
     index = 0
     file = os.listdir(save_path)
     file_num = len(file)
-    m = read_data(n)  # m 中有N个矩阵
+    m = read_data(n)
     np_data = np.array(m)  # N, H, W
     for data in np_data:
         file_num += 1
@@ -90,7 +88,6 @@ def draw(n):
 
 
 if __name__ == '__main__':
-    # 最大文件数量
     max_file = get_max_file(read_path)
     start = 1
     for i in range(start, max_file + 1):
